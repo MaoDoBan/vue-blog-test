54517ae64ed0d91657a93ff3d2f103eac5a87b71
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-27 08:31:09 -0300

    preparo inicial e primeiro uso do setup() no Home

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p>Meu nome é {{ nome }} e minha idade é {{ idade }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  setup(){ //roda antes dos livecycle hooks
+    let nome = "Mario";
+    let idade = 42;
+    return {nome, idade};
+  }
+}
+</script>
