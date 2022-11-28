e27608ca8fae8dcbfd2e06f3871a6ea96933b78f
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-11-28 13:24:37 -0300

    template ref dentro do setup()

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="home">
-    <p>Meu nome é {{ nome }} e minha idade é {{ idade }}</p>
+    <p ref="texto">Meu nome é {{ nome }} e minha idade é {{ idade }}</p>
     <button @click="clicar">Aperte</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'Home',
   setup(){ //roda antes dos livecycle hooks
+    const texto = ref(null);
+
     let nome = "Mario";
     let idade = 42;
 
     function clicar(){
       console.log("clicou!");
+      texto.value.textContent = "Bah dai né, tava frio";
     }
 
-    return {nome, idade, clicar};
+    return {nome, idade, clicar, texto};
   }
 }
 </script>
